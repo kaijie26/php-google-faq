734b317c2926573d3422c4e9dab406ce7a5315f8
kaijie26 <chenkaijie26@gmail.com>
2022-07-25 15:52:16 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <header>
+        <!-- Logo -->
+        <div class="logo">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/2560px-Google_2015_logo.svg.png" alt="google-logo">
+        </div>
+        <h2>
+            Privacy & Termini
+        </h2>
+
+        <!-- Nav-Bar -->
+        <nav class="nav-bar">
+            <!-- List-Menu -->
+            <ul class="list-menu">
+                <li>
+                    <a href="#">Introduzione</a>
+                </li> 
+                <li>
+                    <a href="#">Norme sulla privacy</a>
+                </li> 
+                <li>
+                    <a href="#">Termini di servizio</a>
+                </li> 
+                <li>
+                    <a href="#">Tecnologie</a>
+                </li> 
+                <li>
+                    <a class="current" href="#">Domande frequenti</a>
+                </li> 
+                
+            </ul>
+
+        </nav>
+
+        <hr>
+
+    </header>
+    
+</body>
+</html>
